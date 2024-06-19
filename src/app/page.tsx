860695a826691f5eeafba9df2f3347dd4b103fdf
BCCheungGit <cheungbenjamin0@gmail.com>
2024-06-19 12:10:06 -0400

    now dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { db } from "~/server/db";
 import Link from "next/link";
 
 
+export const dynamic = "force-dynamic";
+
+
 const mockUrls = [
   "https://utfs.io/f/fb1e1817-5448-4ce5-837f-b72ddb1babbd-qxabi4.png",
   "https://utfs.io/f/5ae41a7d-694b-4069-93c5-2f81efe65723-js9elp.jpg"
