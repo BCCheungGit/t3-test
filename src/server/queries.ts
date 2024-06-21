2a729a07d19c62649448868260bcfac5953294c8
BCCheungGit <cheungbenjamin0@gmail.com>
2024-06-21 17:52:40 -0400

    added posthog to delete

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,6 +5,7 @@ import { and, eq } from "drizzle-orm";
 import { images } from "./db/schema";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 
 export async function getMyImages() {
@@ -44,6 +45,14 @@ export async function deleteImage(id: number) {
 
 
     await db.delete(images).where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+    analyticsServerClient.capture({
+        distinctId: user.userId,
+        event: "delete_image",
+        properties: {
+            image_id: id,
+        }
+    })
 
     redirect("/");
 }
